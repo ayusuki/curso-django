e7e8579ef9cb15016bdbc0abe6666237c25a3c43
Andre Yusuki <andre.yusuki@bsgi.org.br>
2012-06-25 15:55:35 -0300

    alteração

--- magnet/noticiario/urls.py
+++ magnet/noticiario/urls.py
@@ -1,11 +1,12 @@
+#coding: utf-8
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
+from django.views.generic import DetailView
 
 from .views import HomeView
-
+from .models import Noticia 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', HomeView.as_view(), name='home'),
-    
+    url(r'^news/(?P<pk>\d+)$', DetailView.as_view(model=Noticia), name='noticia-detalhe'),    
 )
 
