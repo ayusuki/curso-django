f4c07cc25cc4b44f8353ce9e51257abd9250c2e3
Andre Yusuki <andre.yusuki@bsgi.org.br>
2012-06-25 17:47:39 -0300

    capas de canais funcionando

--- magnet/noticiario/urls.py
+++ magnet/noticiario/urls.py
@@ -5,8 +5,11 @@ from django.views.generic import DetailView
 from .views import HomeView
 from .models import Noticia 
 urlpatterns = patterns('',
+    
     # Examples:
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^(?P<secao>\w+)/$', HomeView.as_view(), name='capa-secao'),    
     url(r'^news/(?P<pk>\d+)$', DetailView.as_view(model=Noticia), name='noticia-detalhe'),    
+
 )
 
