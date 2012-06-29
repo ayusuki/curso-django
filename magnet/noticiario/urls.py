206982b020d5f54edd08d91a76b6cdf92030da4c
Andre Yusuki <andre.yusuki@bsgi.org.br>
2012-06-29 17:54:02 -0300

    paginaçao de listas por seção funcionando

--- magnet/noticiario/urls.py
+++ magnet/noticiario/urls.py
@@ -2,13 +2,14 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import DetailView
 
-from .views import HomeView
+from .views import HomeView,ListaSecaoView
 from .models import Noticia 
 urlpatterns = patterns('',
     
     # Examples:
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^(?P<secao>\w+)/$', HomeView.as_view(), name='capa-secao'),    
+    url(r'^(?P<secao>\w+)/todas/$', ListaSecaoView.as_view(), name='lista-secao'),    
     url(r'^news/(?P<pk>\d+)$', DetailView.as_view(model=Noticia), name='noticia-detalhe'),    
 
 )
